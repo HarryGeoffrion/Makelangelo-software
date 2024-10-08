80e9143a2e5af50e6cfe4b13c02533c8377925e3
Harry Geoffrion <62405901+HarryGeoffrion@users.noreply.github.com>
2024-10-08 17:41:38 -0400

    Added screenToWorldSpace test

--- src/test/java/com/marginallyclever/makelangelo/preview/CameraTest.java
+++ src/test/java/com/marginallyclever/makelangelo/preview/CameraTest.java
@@ -5,6 +5,7 @@ import com.marginallyclever.convenience.Point2D;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+
 public class CameraTest {
 
     private Camera camera;
@@ -34,4 +35,54 @@ public class CameraTest {
         camera.zoom(Integer.MAX_VALUE);
         assertEquals(Camera.CAMERA_ZOOM_MIN, camera.getZoom()); // should be 1000.0
     }
+
+    @Test
+    public void testScreenToWorldSpace() {
+
+        // Equation: output.xy = input.xy/zoom + offsetXY;
+
+
+        // ARRANGE - create input data
+        Point2D input = new Point2D(100, 100);
+        // ACT
+        Point2D output = camera.screenToWorldSpace(input);
+        // ASSERT - position is (100, 100) so equation should be:
+        // output.xy = 100.0/1.0 + 0.0;
+        assertEquals(input.x, output.x);
+        assertEquals(input.y, output.y);
+
+
+        // ARRANGE - move camera to test offset
+        camera.moveRelative(100.0, 100.0);
+        // ACT
+        output = camera.screenToWorldSpace(input);
+        // ASSERT - offset is (100, 100) so equation should be:
+        // output.xy = 100.0/1.0 + 100.0;
+        assertEquals(input.x + 100.0, output.x);
+        assertEquals(input.y + 100.0, output.y);
+        
+
+        // ARRANGE - zoom camera to test zoom
+        camera.moveRelative(-100.0, -100.0); // reset position
+        camera.zoom(int(-9.0/Camera.ZOOM_STEP_SIZE));
+        // ACT
+        output = camera.screenToWorldSpace(input);
+        // ASSERT - zoom is (1 + 9) = 10 so equation should be:
+        // output.xy = 100.0/10.0 + 0.0;
+        assertEquals(input.x / 10.0, output.x);
+        assertEquals(input.y / 10.0, output.y);
+
+
+
+        // ARRANGE - move camera to test offset and zoom simultaneously
+        camera.moveRelative(100.0, 100.0);
+        camera.zoom(int(-9.0/Camera.ZOOM_STEP_SIZE));
+        // ACT
+        output = camera.screenToWorldSpace(input);
+        // ASSERT - offset is (100, 100) and zoom is 10 so equation should be:
+        // output.xy = 100.0/10.0 + 100.0;
+        assertEquals(input.x / 10.0 + 100.0, output.x);
+        assertEquals(input.y / 10.0 + 100.0, output.y);
+    }
+
 }
