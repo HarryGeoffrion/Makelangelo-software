8776da3cdae24d8b16a6c31cb193423bd7eb5caf
Harry Geoffrion <62405901+HarryGeoffrion@users.noreply.github.com>
2024-10-08 16:51:21 -0400

    Created CameraTest.java and added initial zoom tests

---
+++ src/test/java/com/marginallyclever/makelangelo/preview/CameraTest.java
@@ -0,0 +1,38 @@
+import static org.junit.jupiter.api.Assertions.*;
+import com.marginallyclever.convenience.Point2D;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+package com.marginallyclever.makelangelo.preview;
+
+
+public class CameraTest {
+
+    private Camera camera;
+
+    @BeforeEach
+    public void setUp() {
+        camera = new Camera();
+    }
+
+    @Test
+    public void testZoomOut() {
+        camera.zoom(-0.5);
+        assertEquals(0.5, camera.getZoom());
+    }
+
+    @Test
+    public void testZoomIn() {
+        camera.zoom(1);
+        assertEquals(2.0, camera.getZoom());
+    }
+
+    @Test
+    public void testZoomMinAndMax() {
+        camera.zoom(-2000);
+        assertEquals(Camera.CAMERA_ZOOM_MIN, camera.getZoom()); // should be 0.25
+
+        camera.zoom(2000);
+        assertEquals(Camera.CAMERA_ZOOM_MAX, camera.getZoom()); // should be 1000.0
+    }
+}
